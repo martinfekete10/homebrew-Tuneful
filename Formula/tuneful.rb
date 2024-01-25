fa10e0ff8db4ce537ec6229dbeccfd54fb5d0fc3
Martin Fekete <martinfekete10@gmail.com>
2024-01-25 22:00:32 +0100

    update

--- Formula/tuneful.rb
+++ Formula/tuneful.rb
@@ -1,11 +1,11 @@
 class Tuneful < Formula
   desc "Native macOS menu bar playback control app for Spotify and Apple Music"
   homepage "https://github.com/martinfekete10/Tuneful"
-  url "https://github.com/martinfekete10/Tuneful/releases/latest/download/Tuneful.tar.gz"
+  url "https://github.com/martinfekete10/Tuneful/releases/download/v1.2.0/Tuneful.tar.gz"
   sha256 "f360663290d0aeb01ab23112c508819f1ab7931b2ae196a8472a1433ad41cfd8"
   version "1.2.0"
 
   def install
-    bin.install "Tuneful.app"
+    app "Tuneful.app"
   end
 end
